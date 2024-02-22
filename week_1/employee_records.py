2c01adf5fc32c778bb4afb030c26ea6b93e727e7
mwaurabridgett@gmail.com <mwaurabridgett@gmail.com>
2024-02-22 10:12:25 +0300

    lessons on strings,loops

---
+++ week_1/employee_records.py
@@ -0,0 +1,25 @@
+# This is a program to create employee data
+# Date : 21/02/2024
+# Name : Bridgett Mwaura
+
+# Employee name : Edward Karanja
+# Age of the employee : '42'
+# Salary of empolyee : 900000
+# Bonus of employee : 80000
+
+# Increase salary by 30%
+salary = int(900000)
+new_salary = salary + (salary * 30/100)
+
+# Decrease bonus by 5000
+bonus = int(80000)
+new_bonus = bonus - int(5000)
+
+print("the new salary is :",new_salary)
+print("the new bonus is :",new_bonus)
+
+
+
+
+
+
